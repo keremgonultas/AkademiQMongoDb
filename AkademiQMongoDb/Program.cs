30221ebbfe74f34672bc2d21c3833ceea9829c8c
Kerem Gönültaş <keremgonultas@ogr.eskisehir.edu.tr>
2026-02-18 01:31:01 +0300

    UI ayarlanmış hali

--- AkademiQMongoDb/Program.cs
+++ AkademiQMongoDb/Program.cs
@@ -6,21 +6,13 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
-
 builder.Services.AddScoped<ICategoryService, CategoryService>();
-
 builder.Services.AddScoped<IProductService, ProductService>();
-
-
-
 builder.Services.AddSingleton<IDatabaseSettings>(sp =>
 {
     return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
 });
-
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -29,28 +21,27 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 
+
+app.UseStaticFiles();
+
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-
-    app.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
 
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
+
+app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
