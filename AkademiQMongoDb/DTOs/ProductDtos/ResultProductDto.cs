2033a391895ce468221594b4be4d6d2cab344cae
Kerem Gönültaş <keremgonultas@ogr.eskisehir.edu.tr>
2026-02-18 22:43:28 +0300

    Ana yemek ve tatlıları ayırmadan önce

--- AkademiQMongoDb/DTOs/ProductDtos/ResultProductDto.cs
+++ AkademiQMongoDb/DTOs/ProductDtos/ResultProductDto.cs
@@ -8,7 +8,7 @@
 
         public string Description { get; set; }
 
-        public string Imageurl { get; set; }
+        public string ImageUrl { get; set; }
 
         public decimal Price { get; set; }
 
