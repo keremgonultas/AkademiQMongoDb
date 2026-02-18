7f3fcb68c8eba07102c5feecde0cb871f00fca0c
Kerem Gönültaş <keremgonultas@ogr.eskisehir.edu.tr>
2026-02-18 19:58:00 +0300

    ui

---
+++ AkademiQMongoDb/ViewComponents/DefaultComponents/_DefaultProductsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using AkademiQMongoDb.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents.DefaultComponents
+{
+    public class _DefaultProductsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public _DefaultProductsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = await _productService.GetAllAsync();
+            return View(products); 
+        }
+    }
+}
