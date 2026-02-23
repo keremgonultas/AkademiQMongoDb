859ef457bf959590865de3698f30f58fdd410a02
Kerem Gönültaş <keremgonultas@ogr.eskisehir.edu.tr>
2026-02-23 17:39:25 +0300

    Navbar ve Footer

---
+++ AkademiQMongoDb/ViewComponents/UILayoutComponents/_UILayoutNavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQMongoDb.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutNavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
